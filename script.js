9ccd6c293dcf7df0ed3d6baba696e0a9dbd66acd
Grace Lin <gracelin3295@gmail.com>
2021-12-13 13:38:57 -0500

    cleaned: fixed up some code

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ let video = document.getElementById("video");
 console.log(video);
 function startVideo() {
   navigator.mediaDevices
-    .getUserMedia(
-      { video: true }
-      // (stream) => (video.srcObject = stream),
-      // (e) => console.error(e)
-    )
+    .getUserMedia({ video: true })
     .then((res) => (video.srcObject = res))
     .catch((err) => console.log(err));
 }
