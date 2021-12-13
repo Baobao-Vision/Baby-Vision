5e868c652487687711b4d733440fb58b60752eba
Grace Lin <gracelin3295@gmail.com>
2021-12-13 15:25:12 -0500

    added: facial detection

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 let video = document.getElementById("video");
-console.log(video);
+
+//models
+Promise.all([
+  //face detector
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  //detects the features of the face
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  //detects the box around the face
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  //detects the facial expression
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
+
 function startVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true })
-    .then((res) => (video.srcObject = res))
+    .then((stream) => (video.srcObject = stream))
     .catch((err) => console.log(err));
 }
-startVideo();
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
